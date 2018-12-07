4c3e749be8a2b89bca0c7821f6f1a75a2786c09a
y.kalinichenko <y.kalinichenko@KALINICHEN-PC>
2018-12-07 15:13:00 +0200

    Добавьте файлы проекта.

---
+++ DataBaseSchema/DataSetActions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataBaseSchema
+{
+    public class DataSetActions
+    {
+        public static string ConnectionString { get; set; }
+        static List<string> tablesName;
+        public static void CreateDataSet(DataSet ds, string connectionString)
+        {
+            DataSetActions.ConnectionString = connectionString;
+            tablesName = GetTablesName(DataSetActions.ConnectionString);
+            FillDataSetTables(ds, tablesName);
+            FillShemaDataSet(ds, DataSetActions.ConnectionString);
+        }
+
+        public static List<string> GetTablesName(string connString)
+        {
+            List<string> tables = new List<string>();
+            DataTable dt;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                dt = conn.GetSchema("Tables");
+            }
+            if (dt == null) throw new Exception("Не удалось получить список таблиц базы данных");
+            foreach (DataRow row in dt.Rows)
+            {
+                string tablename = (string)row[2];
+                tables.Add(tablename);
+            }
+            tables.Sort((string x, string y)=> { return x.CompareTo(y); });
+            return tables;
+        }
+
+        public static void FillDataSetTables(DataSet ds, List<string> tablesName)
+        {
+            foreach (string tn in tablesName)
+            {
+                DataTable table = new DataTable(tn);
+                ds.Tables.Add(table);
+            }
+        }
+        public static void GetSchemaTable(string connString, string tableName, DataSet ds)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = new SqlCommand(string.Format("select * from {0}", tableName), conn);
+                conn.Open();
+                adapter.FillSchema(ds, SchemaType.Source, tableName);
+            }
+        }
+        public static void FillShemaDataSet(DataSet ds, string connString)
+        {
+            foreach (DataTable t in ds.Tables)
+            {
+                GetSchemaTable(connString, t.TableName, ds);
+            }
+        }
+    }
+}
+
---
+++ Tests/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataBaseSchema;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet ds = new DataSet("DefaultDataSetName");
+            string connString = "DefaultConnectionString";
+            DataSetActions.CreateDataSet(ds, connString);
+            //PrintDataSetSchema(ds, "D:\\MainGetonSchema.txt");
+           //DataBaseActions.CreateDataBase(ds, connString, DataBaseActions.SetTransformToSqlTypes());
+        }
+        static void PrintDataSetSchema(DataSet ds, string fileName)
+        {
+            StringBuilder schema = new StringBuilder();
+            schema.AppendLine(string.Format("Count tables: {0}", ds.Tables.Count));
+            foreach (DataTable t in ds.Tables)
+            {
+                schema.AppendLine(string.Format("Table: {0}", t.TableName));
+                schema.AppendLine(string.Format(new string('-', 32)));
+                foreach (DataColumn c in t.Columns)
+                {
+                    schema.AppendLine(string.Format("Column NAME: {0}, TYPE: {1}, IS UNIQUE: {2}\n", c.ColumnName, c.DataType, c.Unique));
+                }
+                schema.AppendLine(string.Format(new string('\n', 3)));
+            }
+            StreamWriter sw = new StreamWriter(fileName);
+            sw.Write(schema.ToString().ToArray<char>());
+        }
+
+    }
+}
